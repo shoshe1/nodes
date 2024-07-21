7f2bcf921ce8321e9c84cbf300a4ee3cf9dacfb8
shoshe1 <shahdbaloum2006@gmail.com>
2024-07-21 23:39:29 +0300

    i update the functions and added the new routers

--- server/models/usermodel.js
+++ server/models/usermodel.js
@@ -2,16 +2,27 @@ const {createConnection} = require('../db');
 
 async function getuserbyusername(username) {
     const connection = await createConnection();
-    const [rows] = await connection.query`'SELECT * FROM tbl_60_users WHERE username ='${username}'`;
-    await connection.end();
+   try{ const [rows] = await connection.query`'SELECT * FROM tbl_60_users WHERE username ='${username}'`;
+    
     return rows[0];
+   } catch (error) {
+       console.error('error in getuserbyusername',error);
+   }
+   finally{
+       await connection.end();
+   }
    
 } 
 async function createuser(username, password , accesscode){
     const connection = await createConnection();
-    const [rows] = await connection.query('INSERT INTO tbl_60_users (username, password, accesscode) VALUES (?, ?, ?)', [username, password, accesscode]);
-    await connection.end();
-    return rows.insertId;
+    try{ await connection.query('INSERT INTO tbl_60_users (username, password, accesscode) VALUES (?, ?, ?)', [username, password, accesscode]);
+
+    } catch (error) {
+        console.error('error in createuser',error);
+    }
+    finally{
+        await connection.end();
+    }
 
 }
 
--- server/routers/preferenceroutes.js
+++ server/routers/preferenceroutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.get("/:userid", preferencecontroller.getpreferencesbyuserid);
 router.post("/", preferencecontroller.updatepreference);
-
+router.get('/', preferencecontroller.getallpreferences);
+router.get('/compute', preferencecontroller.computevacation);
 module.exports = router;
