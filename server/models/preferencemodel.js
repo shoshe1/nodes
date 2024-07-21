b760352739f5cf3e4300b6f0cb17a5fd0ba90f96
shoshe1 <shahdbaloum2006@gmail.com>
2024-07-21 16:26:37 +0300

    i add an error handling to catch and log issues

--- server/models/preferencemodel.js
+++ server/models/preferencemodel.js
@@ -11,5 +11,13 @@ async function getpreferencebyuserid(userid) {
 async function createpreference(userid,startdate,enddate, destination, vacationtype) {
     const connection = await createConnection();
     const [existingpreference] = await connection.execute`'SELECT * FROM tbl_60_preferences WHERE userid ='${userid}'`;
-    
+    if (existingpreference.length >0){
+        const updatequery = `UPDATE tbl_60_preferences SET startdate = '${startdate}', enddate = '${enddate}', destination = '${destination}', vacationtype = '${vacationtype}' WHERE userid = '${userid}'`;
+        await connection.execute(updatequery);
+
+    } else {
+        const insertquery = `INSERT INTO tbl_60_preferences(userid,startdate, enddate, destination, vacationtype) VALUES ('${userid}','${startdate}','${enddate}','${destination}','${vacationtype}')`;
+        await connection.execute(insertquery);
+    }
+    await connection.end();
 }
