a7b2d974744ee1c01297011eb46ae598159cfa9b
shoshe1 <shahdbaloum2006@gmail.com>
2024-07-21 16:45:01 +0300

    i added 2 function one to get preference by user id it handle get requestsand responce the preference for a specific user and handle the error the other function is update the user preference

---
+++ server/controllers/preferencescontroller.js
@@ -0,0 +1,25 @@
+const preferencemodel = require('../models/preferencemodel');
+
+
+async function getpreferencesbyuserid(req, res) {
+    const userid = req.params.userid;
+    try {
+        const preferences = await preferencemodel.getpreferencebyuserid(userid);
+        res.status(200).json(preferences);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
+async function updatepreference(req, res) {
+  
+    const {userid,startdate,enddate, destination, vacationtype} = req.body;
+    try {
+        await preferencemodel.createpreference(userid,startdate,enddate, destination, vacationtype);
+        res.status(200).json({message: 'Preferences updated successfully'});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
+module.exports = {getpreferencesbyuserid, updatepreference}
