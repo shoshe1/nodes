16f4b3e431613dece508d29340d5fbeeefd2c847
shoshe1 <shahdbaloum2006@gmail.com>
2024-07-21 16:38:20 +0300

    adding the function creatconnection to creat a connection to the database from the .env file if it connect succesfuly so it send a success message and if not so it will send an error message

--- server/db.js
+++ server/db.js
@@ -1,3 +1,21 @@
 
 const mysql = require('mysql2/promise');
 require('dotenv').config(); 
+
+async function createConnection() {
+    let connection;
+    try {
+        connection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME
+        });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.error('Error establishing database connection:', error);
+    }
+    return connection;
+}
+
+exports.createConnection = createConnection;
