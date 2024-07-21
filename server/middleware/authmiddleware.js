2b59a241a816fae13e74518bde455cfb59505d76
shoshe1 <shahdbaloum2006@gmail.com>
2024-07-21 17:06:41 +0300

    adding a function that authencate user by the access code and then i reterive the user info from the db using the access cod and attaching the user object to the request object so the middleware can access authenticated user's detailes and also handling the errors

---
+++ server/middleware/authmiddleware.js
@@ -0,0 +1,19 @@
+const usermodel = require('../models/usermodel');
+
+async function authenticateuser(req, res, next) {
+    const {accesscode} = req.body;
+    try {
+        const user = await usermodel.getuserbyaccesscode(accesscode);
+        if(!user) {
+            return res.status(401).json({message: 'Invalid access code'});
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
+
+module.exports = {authenticateuser}
